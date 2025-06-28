6eb0f0f601eb91d355e5cf58fa8d8a3fe1301f05
Karam Abbas <karamabbas03@gmail.com>
2025-06-28 13:48:06 +0500

    Initialized the repo with next

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700", "500"],
+});
+
+export const metadata: Metadata = {
+  title: "Veridxi",
+  description: "Your companion in personal practice",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
